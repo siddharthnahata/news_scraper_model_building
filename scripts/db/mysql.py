7d0eac946a4e71297cdf51e0138724cfaf451db7
Siddharth <siddharthnahata492@gmail.com>
2026-01-24 22:54:53 +0530

    fixed sql env

--- scripts/db/mysql.py
+++ scripts/db/mysql.py
@@ -3,8 +3,8 @@ import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "news_user"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME", "news_engine"),
+        host=os.getenv("NEWS_DB_HOST"),
+        user=os.getenv("NEWS_DB_USER"),
+        password=os.getenv("NEWS_DB_PASSWORD"),
+        database=os.getenv("NEWS_DB_NAME")
     )
